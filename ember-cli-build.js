58fba8b74e0364e515d684c3f28e36bfb9115c2f
Kate White <white.kate.e@gmail.com>
2015-08-19 18:45:40 -0600

    Remove bootstrap for a heroku build test

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,6 +18,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.min.css');
+  //app.import(app.bowerDirectory + '/bootstrap/dist/css/bootsrap.min.css');
   return app.toTree();
 };
