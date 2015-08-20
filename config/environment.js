d5ec20b8824656a7b469e0e475ac3bc1139ef274
Kate White <white.kate.e@gmail.com>
2015-08-19 18:10:20 -0600

    Initial Portfolio

--- config/environment.js
+++ config/environment.js
@@ -6,6 +6,17 @@ module.exports = function(environment) {
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self' 'unsafe-inline' 'unsafe-eval' use.typekit.net",
+      'font-src': "'self' use.typekit.net p.typekit.net data:",
+      'connect-src': "'self'",
+      'img-src': "'self' p.typekit.net",
+      'style-src': "'self' 'unsafe-inline' use.typekit.net p.typekit.net"
+    },
+    typekit: {
+        kitId: 'dgq4kqw'
+    },
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
